6247e51dd4b2b56ad23bbd9a05fc05cd7b4b8c25
Kevin <kevin@kvnd.me>
2015-02-25 20:41:06 -0600

    Popovers and such

--- client/ballot_list.js
+++ client/ballot_list.js
@@ -3,3 +3,75 @@ Template.ballotList.helpers({
         return Ballot.find({}, {sort: {points: -1}})
     }
 });
+
+Template.person.helpers({
+    elementID: function() {
+        return this._id + "_id"
+    },
+    ballotKeys: function() {
+        var keys = [];
+        for (var key in this.first) {
+            keys.push({
+                _id: this._id,
+                key: key
+            });
+        }
+
+        return keys
+    },
+    categoryString: function() {
+        return UI._globalHelpers.Schemas.AnswersScheme.label(this.key.toString())
+    },
+    categorySelectionFirstString: function() {
+        var baloot = Ballot.findOne(this._id);
+        var answer = baloot.first[this.key];
+
+        var winner = Winner.findOne();
+        var actualWinner = winner[this.key];
+
+        var displayAnswer = answer.split(", ")[0].trim();
+
+        if (actualWinner == answer) {
+            displayAnswer += " <span class=\"glyphicon glyphicon-ok\" style=\"color:green\"></span>";
+        }
+
+        return displayAnswer;
+    },
+    categorySelectionSecondString: function() {
+        var baloot = Ballot.findOne(this._id);
+        var answer = baloot.second[this.key];
+
+        var winner = Winner.findOne();
+        var actualWinner = winner[this.key];
+
+        var displayAnswer = answer.split(", ")[0].trim();
+
+        if (actualWinner == answer) {
+            displayAnswer += " <span class=\"glyphicon glyphicon-ok\" style=\"color:green\"></span>";
+        }
+
+        return displayAnswer;
+    }
+});
+
+Template.person.rendered = function() {
+    var divID = $("#" + this.data._id);
+    var test = divID.html();
+    $('.popover_link').popover({
+        container: "body",
+        html: true,
+        trigger: "focus",
+        content: function() {
+            var divID = "#" + this.id.replace("_id", "");
+            return $(divID).html();
+        }
+    });
+};
+
+Template.person.events({
+    'click .list-group-item': function(e, t) {
+        e.popover({
+
+        }).popover('show');
+    }
+});
--- client/ballot_new.js
+++ client/ballot_new.js
@@ -27,11 +27,27 @@ Template.ballotEdit.helpers({
 AutoForm.hooks({
     newForm: {
         onSubmit: function (insertDoc, updateDoc, currentDoc) {
-            Meteor.call('ballotInsert', insertDoc, function(error, result) {
-                Router.go('ballotEdit', {_id: result._id});
-            });
-            this.done();
+            if (!insertDoc.name) {
+                this.done({nameError: true});
+            } else {
+                Meteor.call('ballotInsert', insertDoc, function(error, result) {
+                    if (error) {
+                        alert(error.reason);
+                    } else {
+                        Router.go('ballotEdit', {_id: result._id});
+                    }
+                });
+                this.done();
+            }
+
             return false;
+        },
+        onError: function(operation, error, template) {
+            if (error.nameError) {
+                alert("You forgot to put in your name");
+            } else {
+                alert("Something went wrong. Try again or bug Kevin");
+            }
         }
     }
 });
